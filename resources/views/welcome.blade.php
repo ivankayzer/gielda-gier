e5de98b0687c7d9f5212d63150df86b09532f8ae
Ivan Kayzer <ivan.kayzer@outlook.com>
2018-10-20 16:57:25 +0200

    Add home page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,4 +2,373 @@
 
 @section('content')
 
+
+    <!-- Intro Banner
+================================================== -->
+    <!-- add class "disable-gradient" to enable consistent background overlay -->
+    <div class="intro-banner" data-background-image="images/home-background.jpg">
+        <div class="container">
+
+            <!-- Intro Headline -->
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="banner-headline">
+                        <h3>
+                            <strong>Hire experts or be hired for any job, any time.</strong>
+                            <br>
+                            <span>Thousands of small businesses use <strong class="color">Hireo</strong> to turn their ideas into reality.</span>
+                        </h3>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Search Bar -->
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="intro-banner-search-form margin-top-95">
+
+                        <!-- Search Field -->
+                        <div class="intro-search-field with-autocomplete">
+                            <label for="autocomplete-input" class="field-title ripple-effect">Where?</label>
+                            <div class="input-with-icon">
+                                <input id="autocomplete-input" type="text" placeholder="Online Job">
+                                <i class="icon-material-outline-location-on"></i>
+                            </div>
+                        </div>
+
+                        <!-- Search Field -->
+                        <div class="intro-search-field">
+                            <label for ="intro-keywords" class="field-title ripple-effect">What job you want?</label>
+                            <input id="intro-keywords" type="text" placeholder="Job Title or Keywords">
+                        </div>
+
+                        <!-- Button -->
+                        <div class="intro-search-button">
+                            <button class="button ripple-effect" onclick="window.location.href='jobs-list-layout-full-page-map.html'">Search</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Stats -->
+            <div class="row">
+                <div class="col-md-12">
+                    <ul class="intro-stats margin-top-45 hide-under-992px">
+                        <li>
+                            <strong class="counter">1,586</strong>
+                            <span>Jobs Posted</span>
+                        </li>
+                        <li>
+                            <strong class="counter">3,543</strong>
+                            <span>Tasks Posted</span>
+                        </li>
+                        <li>
+                            <strong class="counter">1,232</strong>
+                            <span>Freelancers</span>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+    <!-- Icon Boxes -->
+    <div class="section padding-top-65 padding-bottom-65">
+        <div class="container">
+            <div class="row">
+
+                <div class="col-xl-12">
+                    <!-- Section Headline -->
+                    <div class="section-headline margin-top-0 margin-bottom-5">
+                        <h3>How It Works?</h3>
+                    </div>
+                </div>
+
+                <div class="col-xl-4 col-md-4">
+                    <!-- Icon Box -->
+                    <div class="icon-box with-line">
+                        <!-- Icon -->
+                        <div class="icon-box-circle">
+                            <div class="icon-box-circle-inner">
+                                <i class="icon-line-awesome-lock"></i>
+                                <div class="icon-box-check"><i class="icon-material-outline-check"></i></div>
+                            </div>
+                        </div>
+                        <h3>Create an Account</h3>
+                        <p>Bring to the table win-win survival strategies to ensure proactive domination going forward.</p>
+                    </div>
+                </div>
+
+                <div class="col-xl-4 col-md-4">
+                    <!-- Icon Box -->
+                    <div class="icon-box with-line">
+                        <!-- Icon -->
+                        <div class="icon-box-circle">
+                            <div class="icon-box-circle-inner">
+                                <i class="icon-line-awesome-legal"></i>
+                                <div class="icon-box-check"><i class="icon-material-outline-check"></i></div>
+                            </div>
+                        </div>
+                        <h3>Post a Task</h3>
+                        <p>Efficiently unleash cross-media information without. Quickly maximize return on investment.</p>
+                    </div>
+                </div>
+
+                <div class="col-xl-4 col-md-4">
+                    <!-- Icon Box -->
+                    <div class="icon-box">
+                        <!-- Icon -->
+                        <div class="icon-box-circle">
+                            <div class="icon-box-circle-inner">
+                                <i class=" icon-line-awesome-trophy"></i>
+                                <div class="icon-box-check"><i class="icon-material-outline-check"></i></div>
+                            </div>
+                        </div>
+                        <h3>Choose an Expert</h3>
+                        <p>Nanotechnology immersion along the information highway will close the loop on focusing solely.</p>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+    <!-- Icon Boxes / End -->
+
+    <!-- Features Cities -->
+    <div class="section gray margin-top-65 padding-top-65">
+        <div class="container">
+            <div class="row">
+
+                <!-- Section Headline -->
+                <div class="col-xl-12">
+                    <div class="section-headline margin-top-0 margin-bottom-45">
+                        <h3>Featured Cities</h3>
+                    </div>
+                </div>
+
+                <div class="col-xl-3 col-md-6">
+                    <!-- Photo Box -->
+                    <a href="jobs-list-layout-1.html" class="photo-box" data-background-image="images/featured-city-01.jpg">
+                        <div class="photo-box-content">
+                            <h3>San Francisco</h3>
+                            <span>376 Jobs</span>
+                        </div>
+                    </a>
+                </div>
+
+                <div class="col-xl-3 col-md-6">
+                    <!-- Photo Box -->
+                    <a href="jobs-list-layout-full-page-map.html" class="photo-box" data-background-image="images/featured-city-02.jpg">
+                        <div class="photo-box-content">
+                            <h3>New York</h3>
+                            <span>645 Jobs</span>
+                        </div>
+                    </a>
+                </div>
+
+                <div class="col-xl-3 col-md-6">
+                    <!-- Photo Box -->
+                    <a href="jobs-grid-layout-full-page.html" class="photo-box" data-background-image="images/featured-city-03.jpg">
+                        <div class="photo-box-content">
+                            <h3>Los Angeles</h3>
+                            <span>832 Jobs</span>
+                        </div>
+                    </a>
+                </div>
+
+                <div class="col-xl-3 col-md-6">
+                    <!-- Photo Box -->
+                    <a href="jobs-list-layout-2.html" class="photo-box" data-background-image="images/featured-city-04.jpg">
+                        <div class="photo-box-content">
+                            <h3>Miami</h3>
+                            <span>513 Jobs</span>
+                        </div>
+                    </a>
+                </div>
+
+            </div>
+        </div>
+    </div>
+    <!-- Features Cities / End -->
+
+    <!-- Features Jobs -->
+    <div class="section gray padding-top-65 padding-bottom-75">
+        <div class="container">
+            <div class="row">
+                <div class="col-xl-12">
+
+                    <!-- Section Headline -->
+                    <div class="section-headline margin-top-0 margin-bottom-35">
+                        <h3>Featured Jobs</h3>
+                        <a href="jobs-list-layout-full-page-map.html" class="headline-link">Browse All Jobs</a>
+                    </div>
+
+                    <!-- Jobs Container -->
+                    <div class="listings-container compact-list-layout margin-top-35">
+
+                        <!-- Job Listing -->
+                        <a href="single-job-page.html" class="job-listing with-apply-button">
+
+                            <!-- Job Listing Details -->
+                            <div class="job-listing-details">
+
+                                <!-- Logo -->
+                                <div class="job-listing-company-logo">
+                                    <img src="images/company-logo-01.png" alt="">
+                                </div>
+
+                                <!-- Details -->
+                                <div class="job-listing-description">
+                                    <h3 class="job-listing-title">Bilingual Event Support Specialist</h3>
+
+                                    <!-- Job Listing Footer -->
+                                    <div class="job-listing-footer">
+                                        <ul>
+                                            <li><i class="icon-material-outline-business"></i> Hexagon <div class="verified-badge" title="Verified Employer" data-tippy-placement="top"></div></li>
+                                            <li><i class="icon-material-outline-location-on"></i> San Francissco</li>
+                                            <li><i class="icon-material-outline-business-center"></i> Full Time</li>
+                                            <li><i class="icon-material-outline-access-time"></i> 2 days ago</li>
+                                        </ul>
+                                    </div>
+                                </div>
+
+                                <!-- Apply Button -->
+                                <span class="list-apply-button ripple-effect">Apply Now</span>
+                            </div>
+                        </a>
+
+
+                        <!-- Job Listing -->
+                        <a href="single-job-page.html" class="job-listing with-apply-button">
+
+                            <!-- Job Listing Details -->
+                            <div class="job-listing-details">
+
+                                <!-- Logo -->
+                                <div class="job-listing-company-logo">
+                                    <img src="images/company-logo-05.png" alt="">
+                                </div>
+
+                                <!-- Details -->
+                                <div class="job-listing-description">
+                                    <h3 class="job-listing-title">Competition Law Officer</h3>
+
+                                    <!-- Job Listing Footer -->
+                                    <div class="job-listing-footer">
+                                        <ul>
+                                            <li><i class="icon-material-outline-business"></i> Laxo</li>
+                                            <li><i class="icon-material-outline-location-on"></i> San Francissco</li>
+                                            <li><i class="icon-material-outline-business-center"></i> Full Time</li>
+                                            <li><i class="icon-material-outline-access-time"></i> 2 days ago</li>
+                                        </ul>
+                                    </div>
+                                </div>
+
+                                <!-- Apply Button -->
+                                <span class="list-apply-button ripple-effect">Apply Now</span>
+                            </div>
+                        </a>
+                        <!-- Job Listing -->
+                        <a href="single-job-page.html" class="job-listing with-apply-button">
+
+                            <!-- Job Listing Details -->
+                            <div class="job-listing-details">
+
+                                <!-- Logo -->
+                                <div class="job-listing-company-logo">
+                                    <img src="images/company-logo-02.png" alt="">
+                                </div>
+
+                                <!-- Details -->
+                                <div class="job-listing-description">
+                                    <h3 class="job-listing-title">Barista and Cashier</h3>
+
+                                    <!-- Job Listing Footer -->
+                                    <div class="job-listing-footer">
+                                        <ul>
+                                            <li><i class="icon-material-outline-business"></i> Coffee</li>
+                                            <li><i class="icon-material-outline-location-on"></i> San Francissco</li>
+                                            <li><i class="icon-material-outline-business-center"></i> Full Time</li>
+                                            <li><i class="icon-material-outline-access-time"></i> 2 days ago</li>
+                                        </ul>
+                                    </div>
+                                </div>
+
+                                <!-- Apply Button -->
+                                <span class="list-apply-button ripple-effect">Apply Now</span>
+                            </div>
+                        </a>
+
+
+                        <!-- Job Listing -->
+                        <a href="single-job-page.html" class="job-listing with-apply-button">
+
+                            <!-- Job Listing Details -->
+                            <div class="job-listing-details">
+
+                                <!-- Logo -->
+                                <div class="job-listing-company-logo">
+                                    <img src="images/company-logo-03.png" alt="">
+                                </div>
+
+                                <!-- Details -->
+                                <div class="job-listing-description">
+                                    <h3 class="job-listing-title">Restaurant General Manager</h3>
+
+                                    <!-- Job Listing Footer -->
+                                    <div class="job-listing-footer">
+                                        <ul>
+                                            <li><i class="icon-material-outline-business"></i> King <div class="verified-badge" title="Verified Employer" data-tippy-placement="top"></div></li>
+                                            <li><i class="icon-material-outline-location-on"></i> San Francissco</li>
+                                            <li><i class="icon-material-outline-business-center"></i> Full Time</li>
+                                            <li><i class="icon-material-outline-access-time"></i> 2 days ago</li>
+                                        </ul>
+                                    </div>
+                                </div>
+
+                                <!-- Apply Button -->
+                                <span class="list-apply-button ripple-effect">Apply Now</span>
+                            </div>
+                        </a>
+
+                        <!-- Job Listing -->
+                        <a href="single-job-page.html" class="job-listing with-apply-button">
+
+                            <!-- Job Listing Details -->
+                            <div class="job-listing-details">
+
+                                <!-- Logo -->
+                                <div class="job-listing-company-logo">
+                                    <img src="images/company-logo-05.png" alt="">
+                                </div>
+
+                                <!-- Details -->
+                                <div class="job-listing-description">
+                                    <h3 class="job-listing-title">International Marketing Coordinator</h3>
+
+                                    <!-- Job Listing Footer -->
+                                    <div class="job-listing-footer">
+                                        <ul>
+                                            <li><i class="icon-material-outline-business"></i> Skyist</li>
+                                            <li><i class="icon-material-outline-location-on"></i> San Francissco</li>
+                                            <li><i class="icon-material-outline-business-center"></i> Full Time</li>
+                                            <li><i class="icon-material-outline-access-time"></i> 2 days ago</li>
+                                        </ul>
+                                    </div>
+                                </div>
+
+                                <!-- Apply Button -->
+                                <span class="list-apply-button ripple-effect">Apply Now</span>
+                            </div>
+                        </a>
+
+                    </div>
+                    <!-- Jobs Container / End -->
+
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Featured Jobs / End -->
 @endsection
