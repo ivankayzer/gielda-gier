14f50f86d62703cbf2c8260928ea32544821c1f8
Ivan Kayzer <ivan.kayzer@outlook.com>
2018-12-09 12:58:24 +0100

    Backups

--- config/app.php
+++ config/app.php
@@ -174,6 +174,7 @@ return [
         App\Providers\TelescopeServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         \App\Providers\ViewComposerProvider::class,
+        App\Providers\GoogleDriveServiceProvider::class,
 
     ],
 
--- config/database.php
+++ config/database.php
@@ -55,6 +55,22 @@ return [
             'engine' => null,
         ],
 
+        'mysql-forum' => [
+            'driver' => 'mysql',
+            'host' => env('DB_HOST_FORUM', '127.0.0.1'),
+            'port' => env('DB_PORT_FORUM', '3306'),
+            'database' => env('DB_DATABASE_FORUM', 'forge'),
+            'username' => env('DB_USERNAME_FORUM', 'forge'),
+            'password' => env('DB_PASSWORD_FORUM', ''),
+            'unix_socket' => env('DB_SOCKET_FORUM', ''),
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+            'prefix' => '',
+            'prefix_indexes' => true,
+            'strict' => true,
+            'engine' => null,
+        ],
+
         'pgsql' => [
             'driver' => 'pgsql',
             'host' => env('DB_HOST', '127.0.0.1'),
