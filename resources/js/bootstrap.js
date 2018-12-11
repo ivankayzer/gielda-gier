7c678dd508548e5c13ec262f01f47eb921e62dc4
Ivan Kayzer <ivan.kayzer@outlook.com>
2018-12-11 17:44:21 +0100

    Switch to pusher

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -40,9 +40,18 @@ if (token) {
 
 import Echo from "laravel-echo"
 
+window.Pusher = require('pusher-js');
+
+/* Socket.io
 window.io = require('socket.io-client');
 
 window.Echo = new Echo({
     broadcaster: 'socket.io',
     host: window.location.hostname + ':6001',
+});
+*/
+
+window.Echo = new Echo({
+    broadcaster: 'pusher',
+    key: '1661904c08d9f0bdf237'
 });
