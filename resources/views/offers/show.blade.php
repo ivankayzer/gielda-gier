e51ffe05204ae19c5a2f8c736bafb5716531b99c
Ivan Kayzer <ivan.kayzer@outlook.com>
2019-04-03 18:54:33 +0200

    Remove share widget

--- resources/views/offers/show.blade.php
+++ resources/views/offers/show.blade.php
@@ -141,40 +141,6 @@
                                 </div>
                             </div>
                         </div>
-
-                        <!-- Sidebar Widget -->
-                        <div class="sidebar-widget">
-                            <h3>@lang('offers.share')</h3>
-
-                            <!-- Copy URL -->
-                            <div class="copy-url">
-                                <input id="copy-url" type="text" value="" class="with-border">
-                                <button class="copy-url-button ripple-effect" data-clipboard-target="#copy-url"
-                                        title="Copy to Clipboard" data-tippy-placement="top"><i
-                                            class="icon-material-outline-file-copy"></i></button>
-                            </div>
-
-                            <!-- Share Buttons -->
-                            <div class="share-buttons margin-top-25">
-                                <div class="share-buttons-trigger"><i class="icon-feather-share-2"></i></div>
-                                <div class="share-buttons-content">
-                                    <span>@lang('offers.share')</span>
-                                    <ul class="share-buttons-icons">
-                                        <li><a href="#" data-button-color="#3b5998" title="Share on Facebook"
-                                               data-tippy-placement="top"><i class="icon-brand-facebook-f"></i></a></li>
-                                        <li><a href="#" data-button-color="#1da1f2" title="Share on Twitter"
-                                               data-tippy-placement="top"><i class="icon-brand-twitter"></i></a></li>
-                                        <li><a href="#" data-button-color="#dd4b39" title="Share on Google Plus"
-                                               data-tippy-placement="top"><i class="icon-brand-google-plus-g"></i></a>
-                                        </li>
-                                        <li><a href="#" data-button-color="#0077b5" title="Share on LinkedIn"
-                                               data-tippy-placement="top"><i class="icon-brand-linkedin-in"></i></a>
-                                        </li>
-                                    </ul>
-                                </div>
-                            </div>
-                        </div>
-
                     </div>
                 </div>
         @endif
@@ -247,41 +213,6 @@
                             <li class="{{ $offer->delivery_post ? 'yes' : 'no' }}">@lang('offers.delivery_post')</li>
                             <li class="{{ $offer->delivery_in_person ? 'yes' : 'no' }}">@lang('offers.delivery_in_person')</li>
                         </ul>
-                    </div>
-                    <div class="sidebar-container">
-                        <!-- Sidebar Widget -->
-                        <div class="sidebar-widget">
-                            <h3>@lang('offers.share')</h3>
-
-                            <!-- Copy URL -->
-                            <div class="copy-url">
-                                <input id="copy-url" type="text" value="" class="with-border">
-                                <button class="copy-url-button ripple-effect" data-clipboard-target="#copy-url"
-                                        title="Copy to Clipboard" data-tippy-placement="top"><i
-                                            class="icon-material-outline-file-copy"></i></button>
-                            </div>
-
-                            <!-- Share Buttons -->
-                            <div class="share-buttons margin-top-25">
-                                <div class="share-buttons-trigger"><i class="icon-feather-share-2"></i></div>
-                                <div class="share-buttons-content">
-                                    <span>@lang('offers.share')</span>
-                                    <ul class="share-buttons-icons">
-                                        <li><a href="#" data-button-color="#3b5998" title="Share on Facebook"
-                                               data-tippy-placement="top"><i class="icon-brand-facebook-f"></i></a></li>
-                                        <li><a href="#" data-button-color="#1da1f2" title="Share on Twitter"
-                                               data-tippy-placement="top"><i class="icon-brand-twitter"></i></a></li>
-                                        <li><a href="#" data-button-color="#dd4b39" title="Share on Google Plus"
-                                               data-tippy-placement="top"><i class="icon-brand-google-plus-g"></i></a>
-                                        </li>
-                                        <li><a href="#" data-button-color="#0077b5" title="Share on LinkedIn"
-                                               data-tippy-placement="top"><i class="icon-brand-linkedin-in"></i></a>
-                                        </li>
-                                    </ul>
-                                </div>
-                            </div>
-                        </div>
-
                     </div>
                 </div>
             @endif
--- resources/views/users/profile.blade.php
+++ resources/views/users/profile.blade.php
@@ -15,7 +15,7 @@
                             <div class="header-details">
                                 <h3>
                                     @if(auth()->check() && $user->id === auth()->id())
-                                    {{ $user->profile->getFullName() }}
+                                        {{ $user->profile->getFullName() }}
                                     @else
                                         {{ $user->name }}
                                     @endif
@@ -32,95 +32,64 @@
         <div class="row">
 
             <!-- Content -->
-            <div class="col-xl-8 col-lg-8 content-right-offset">
+            @if($user->profile->description)
+                <div class="col-xl-8 col-lg-8 content-right-offset">
+                    @else
+                        <div class="col-xl-12 col-lg-12 content-right-offset">
+                            @endif
+                            <div class="boxed-list margin-bottom-60">
+                                <div class="boxed-list-headline">
+                                    <h3><i class="icon-material-outline-thumb-up"></i> @lang('profile.reviews')</h3>
+                                </div>
+                                <ul class="boxed-list-ul">
+                                    @foreach($reviews as $review)
+                                        @include('users._review', ['review' => $review])
+                                    @endforeach
 
-                <div class="boxed-list margin-bottom-60">
-                    <div class="boxed-list-headline">
-                        <h3><i class="icon-material-outline-thumb-up"></i> @lang('profile.reviews')</h3>
-                    </div>
-                    <ul class="boxed-list-ul">
-                        @foreach($reviews as $review)
-                            @include('users._review', ['review' => $review])
-                        @endforeach
-
-                        @if(!count($reviews))
-                            <p class="margin-top-30 margin-left-30">@lang('profile.no_reviews')</p>
-                        @endif
-                    </ul>
-
-                    <!-- Pagination -->
-                    <div class="clearfix"></div>
-                    {{ $reviews->links() }}
-                    <div class="clearfix"></div>
-                    <!-- Pagination / End -->
-
-                </div>
-            </div>
-            <!-- Sidebar -->
-            <div class="col-xl-4 col-lg-4">
-                <div class="sidebar-container">
-
-                    @if($user->profile->description)
-                    <!-- Profile Overview -->
-                    <div class="profile-overview">
-                        <div class="single-page-section">
-                            <h3 class="margin-bottom-25">@lang('profile.about_me')</h3>
-                            <p>{{ $user->profile->description }}</p>
-                        </div>
-                    </div>
-                    @endif
-
-                    @if(auth()->check() && $user->id !== auth()->id())
-                    <!-- Sidebar Widget -->
-                    <div class="sidebar-widget">
-                        <h3>@lang('offers.share')</h3>
-
-                        <!-- Copy URL -->
-                        <div class="copy-url">
-                            <input id="copy-url" type="text" value="" class="with-border">
-                            <button class="copy-url-button ripple-effect" data-clipboard-target="#copy-url"
-                                    title="Copy to Clipboard" data-tippy-placement="top"><i
-                                        class="icon-material-outline-file-copy"></i></button>
-                        </div>
-
-                        <!-- Share Buttons -->
-                        <div class="share-buttons margin-top-25">
-                            <div class="share-buttons-trigger"><i class="icon-feather-share-2"></i></div>
-                            <div class="share-buttons-content">
-                                <span>@lang('offers.share')</span>
-                                <ul class="share-buttons-icons">
-                                    <li><a href="#" data-button-color="#3b5998" title="Share on Facebook"
-                                           data-tippy-placement="top"><i class="icon-brand-facebook-f"></i></a></li>
-                                    <li><a href="#" data-button-color="#1da1f2" title="Share on Twitter"
-                                           data-tippy-placement="top"><i class="icon-brand-twitter"></i></a></li>
-                                    <li><a href="#" data-button-color="#dd4b39" title="Share on Google Plus"
-                                           data-tippy-placement="top"><i class="icon-brand-google-plus-g"></i></a></li>
-                                    <li><a href="#" data-button-color="#0077b5" title="Share on LinkedIn"
-                                           data-tippy-placement="top"><i class="icon-brand-linkedin-in"></i></a></li>
+                                    @if(!count($reviews))
+                                        <p class="margin-top-30 margin-left-30">@lang('profile.no_reviews')</p>
+                                    @endif
                                 </ul>
+
+                                <!-- Pagination -->
+                                <div class="clearfix"></div>
+                                {{ $reviews->links() }}
+                                <div class="clearfix"></div>
+                                <!-- Pagination / End -->
+
                             </div>
                         </div>
-                    </div>
-                    @endif
+                        <!-- Sidebar -->
+                        @if($user->profile->description)
+                            <div class="col-xl-4 col-lg-4">
+                                <div class="sidebar-container">
+                                    <!-- Profile Overview -->
+                                    <div class="profile-overview">
+                                        <div class="single-page-section">
+                                            <h3 class="margin-bottom-25">@lang('profile.about_me')</h3>
+                                            <p>{{ $user->profile->description }}</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        @endif
+                        <div class="col-xl-12 col-lg-12 content-right-offset">
+
+
+                            <div class="boxed-list margin-bottom-60">
+                                <div class="boxed-list-headline">
+                                    <h3><i class="icon-material-outline-business"></i> @lang('profile.offers')</h3>
+                                </div>
+                                <div class="freelancers-container freelancers-list-layout compact-list">
+
+                                    @foreach($offers as $offer)
+                                        @include('offers._offer', ['offer' => $offer])
+                                    @endforeach
+                                </div>
+                            </div>
+
+                        </div>
+
                 </div>
-            </div>
-            <div class="col-xl-12 col-lg-12 content-right-offset">
-
-
-                <div class="boxed-list margin-bottom-60">
-                    <div class="boxed-list-headline">
-                        <h3><i class="icon-material-outline-business"></i> @lang('profile.offers')</h3>
-                    </div>
-                    <div class="freelancers-container freelancers-list-layout compact-list">
-
-                        @foreach($offers as $offer)
-                            @include('offers._offer', ['offer' => $offer])
-                        @endforeach
-                    </div>
-                </div>
-
-            </div>
-
         </div>
-    </div>
 @endsection
