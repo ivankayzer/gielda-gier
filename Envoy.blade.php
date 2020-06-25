946924bf08a94507731242130458bc3ef54cecbf
Ivan Kayzer <ivan.kayzer@outlook.com>
2020-06-25 20:50:07 +0200

    Update Envoy.blade.php

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -1,30 +1,7 @@
-@servers(['web' => 'root@145.239.80.94'])
-
-@task('dev', ['on' => 'web'])
-cd /var/www/beta.gielda-gier.pl
-
-php artisan down
-git fetch origin develop
-git checkout --force develop
-git reset --hard FETCH_HEAD
-git clean -df
-
-composer install --no-interaction --prefer-dist --ignore-platform-reqs
-npm install --only=prod
-
-php artisan migrate
-
-php artisan cache:clear
-php artisan view:clear
-php artisan config:cache
-
-npm run production
-
-php artisan up
-@endtask
+@servers(['web' => 'root@78.46.229.210'])
 
 @task('prod', ['on' => 'web'])
-cd /var/www/gielda-gier.pl
+cd /var/www/gielda-gier
 
 php artisan down
 git fetch origin master
